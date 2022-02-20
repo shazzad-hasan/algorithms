57e4b843b51f6c874992d3b2352af50cdf27c5e4
Shazzad Hasan <shazzadraihan@gmail.com>
2022-02-21 01:14:07 +0600

    add selection sort

---
+++ Sorting and Searching/Selection_Sort.py
@@ -0,0 +1,13 @@
+def selection_sort(L):
+    suffixSt = 0
+    while suffixSt != len(L):
+        for i in range(suffixSt, len(L)):
+            if L[i] < L[suffixSt]:
+                L[suffixSt], L[i] = L[i], L[suffixSt]
+                
+        suffixSt += 1
+        
+L = [54,26,93,17,77,31,44,55,20]
+
+selection_sort(L)
+print(L)
