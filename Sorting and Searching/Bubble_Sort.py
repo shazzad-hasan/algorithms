92aea572c1d6519b93a8de7b144e6b6213c6a363
Shazzad Hasan <shazzadraihan@gmail.com>
2022-02-21 18:07:40 +0600

    added Bubble Sort

---
+++ Sorting and Searching/Bubble_Sort.py
@@ -0,0 +1,20 @@
+def bubble_sort(L):
+    # # while loop implementation
+    swap = False
+    while not swap:
+        swap = True
+        for i in range(1, len(L)):
+            if L[i-1] > L[i]:
+                swap = False
+                L[i-1], L[i] = L[i], L[i-1]
+
+    # for loop implementation
+    # for pass_num in range(len(L), 0, -1):
+    #     for i in range(1, pass_num):
+    #         if L[i-1] > L[i]:
+    #             L[i-1], L[i] = L[i], L[i-1]
+    
+
+testlist = [75,16,83,17,17,77,31,0,44,55,20]
+bubble_sort(testlist)
+print(testlist)
