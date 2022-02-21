aa8792fbabf97eef7c29d5f2be2af8d36917c186
Shazzad Hasan <shazzadraihan@gmail.com>
2022-02-21 14:41:45 +0600

    add Linear Search

--- Sorting and Searching/Linear_Search.py
+++ Sorting and Searching/Linear_Search.py
@@ -1,5 +1,5 @@
 def linear_search(L, e):
-    """ Linear Search on unsorted List """
+    """ Linear search on unsorted list """
     # # using for loop
     # found = False
     # for i in range(len(L)):
@@ -17,6 +17,36 @@ def linear_search(L, e):
             ind += 1 
     return found 
 
-testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
-print(linear_search(testlist, 17))
-print(linear_search(testlist, 1000))
+# testlist = [1, 2, 32, 8, 17, 19, 42, 13, 0]
+# print(linear_search(testlist, 17))
+# print(linear_search(testlist, 1000))
+
+
+
+def Ordered_Linear_Search(L, e):
+    """ Linear search on sorted list """
+    # # using for loop
+    # for i in range(len(L)):
+    #     if L[i] == e:
+    #         return True 
+    #     if L[i] > e:
+    #         return False 
+    # return False 
+
+    # using while loop
+    ind = 0
+    found = False 
+    stop = False 
+    while ind < len(L) and not found and not stop:
+        if L[ind] == e:
+            found = True 
+        else:
+            if L[ind] > e:
+                stop = True 
+            else:
+                ind += 1 
+    return found
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42]
+print(Ordered_Linear_Search(testlist, 17))
+print(Ordered_Linear_Search(testlist, 20))
