8b27b98639704cda7ac3a842080e4f6c8cbce6d1
Shazzad Hasan <shazzadraihan@gmail.com>
2022-03-19 17:25:14 +0600

    added deque

--- linear_data_stractures/deque.py
+++ linear_data_stractures/deque.py
@@ -1,35 +1,35 @@
-class Deque(object):
+class Deque:
     def __init__(self):
         self.items = []
         
     def size(self):
         return len(self.items)
     
-    def isEmpty(self):
+    def is_empty(self):
         return self.items == []
     
-    def addFront(self, item): # adds a new item to the front
+    def add_front(self, item): # adds a new item to the front
         self.items.append(item)
         
-    def addRear(self, item): # adds a new item to the rear
+    def add_rear(self, item): # adds a new item to the rear
         self.items.insert(0, item)
         
-    def removeFront(self): # removes the front item
+    def remove_front(self): # removes the front item
         return self.items.pop()
     
-    def removeRear(self): # removes the rear item
+    def remove_rear(self): # removes the rear item
         return self.items.pop(0)
     
 d = Deque()
-print(d.isEmpty())
+print(d.is_empty())
 print(d.size())
 
-d.addFront('x')
-d.addRear('y')
-print(d.isEmpty())
+d.add_front('x')
+d.add_rear('y')
+print(d.is_empty())
 print(d.size())
 
-d.removeFront()
-d.removeRear()
-print(d.isEmpty())
+d.remove_front()
+d.remove_rear()
+print(d.is_empty())
 print(d.size())
