67b06e87e994b4fe8f20db740915d16390b3432d
Shazzad Hasan <shazzadraihan@gmail.com>
2022-03-22 17:32:59 +0600

    added linear complexity example

---
+++ complexity_classes/linear.py
@@ -0,0 +1,18 @@
+def factorial(n):
+    """ 
+    Take in a number n, assume n>0 and return n!
+    
+    Complexity: O(n)
+    
+    """
+    # using loop
+    # prod = 1
+    # for i in range(1, n+1):
+    #     prod *= i
+    # return prod 
+
+    # recursive version
+    if n <= 1:
+        return 1
+    else:
+        return n * factorial(n-1)
