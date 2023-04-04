5318254453c4923e95c9999ead6da969a4b9dfb9
Shazzad Hasan <shazzadraihan@gmail.com>
2023-04-05 03:54:01 +0600

    updated

--- graph/breadth_first_search.py
+++ graph/breadth_first_search.py
@@ -1,25 +1,72 @@
-def bfs_traversal(graph, start):
-    visited = [start]
-    queue = [start]
+def bfs(graph, start):
+    visited, queue = [start], [start]
+    
     while queue:
-        vertix = queue.pop(0)
+        vertex = queue.pop(0)
+        print(vertex, end = " ")
 
-        for v in graph[vertix]:
+        for v in graph[vertex]:
             if v not in visited:
                 visited.append(v)
                 queue.append(v)
-    return visited
 
 def main():
+    """
+             A
+          /     \
+         B       C
+       /   \     | \
+      D     E    F   G
+            |
+            H
+    """
     g1 = {
         "A": ["B", "C"],
         "B": ["A", "D", "E"],
-        "C": ["A", "F"],
+        "C": ["A", "F", "G"],
         "D": ["B"],
-        "E": ["B", "F"],
-        "F": ["C", "E"],
+        "E": ["B", "H"],
+        "F": ["C"],
+        "G": ["C"],
+        "H": ["E"]
     }
-    print(bfs_traversal(g1, "A"))
+    bfs(g1, "A")
+    
+    print()
+
+    """
+    A --- B -- E
+    | \   |  /
+    |  \  | /
+    C --- D
+    """
+    g2 = {
+        'A': ['B', 'C', 'D'],
+        'B': ['A'],
+        'C': ['A', 'D'],
+        'D': ['A', 'C', 'E'],
+        'E': ['D'],
+    }
+    bfs(g2, "A")
+
+    print()
+
+    """ 
+        A --- B
+        | \
+        |  \
+        C -- D
+              \
+               E
+    """
+    g3 = {
+        'A': ['B', 'C', 'D'],
+        'B': ['A'],
+        'C': ['A', 'D'],
+        'D': ['A', 'C', 'E'],
+        'E': ['D'],
+    }
+    bfs(g3, "A")
 
 if __name__ == "__main__":
     main()
