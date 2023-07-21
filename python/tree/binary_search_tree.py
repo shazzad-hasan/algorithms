9640335cc762e739a8ebb0732354c8012b8159f3
shazzad <shazzadraihan@gmail.com>
2023-07-22 00:13:07 +0600

    updated

--- python/tree/binary_search_tree.py
+++ python/tree/binary_search_tree.py
@@ -1,3 +1,5 @@
+""" Python implementation of binary search tree using recursion """
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -30,27 +32,23 @@ class Binary_Search_Tree:
     def search(self, item):
         """ Searches a node in the tree """
         if self.root.value is None:
-            print(f"Node with item {item} doesn't exist.")
             return False
         else:
             return self._search(self.root, item)
     
     def _search(self, node,  item): 
         if node.value == item:
-            print(f"Node with item {item} exists.")
             return True 
         
         elif item <= node.value:
             if node.left is not None:
                 return self._search(node.left, item)
             else:
-                print(f"Node with item {item} doesn't exist.")
                 return False
         else:
             if node.right is not None:
                 return self._search(node.right, item)
             else:
-                print(f"Node with item {item} doesn't exist.")
                 return False
             
     def remove(self, item):
@@ -62,7 +60,7 @@ class Binary_Search_Tree:
         if self.root.value == item:
             # 1. Node to remove is a leaf node
             if self.root.left is None and self.root.right is None:
-                self.root = None
+                self.root = Node(None)
             # 2. Node to remove has only left subtree
             elif self.root.left is not None and self.root.right is None:
                 self.root = self.root.left 
@@ -72,7 +70,7 @@ class Binary_Search_Tree:
             # 4. Node to remove has a both a left subtree and a right subtree
             else:
                 self.root.value = self._most_left_node_from_right_subtree(self.root.right).value
-                self._remove_item(self.root, self.root.right, self.root.value)
+                self._remove(self.root, self.root.right, self.root.value)
                 
         elif item < self.root.value:
             self._remove(self.root, self.root.left, item)
@@ -102,11 +100,11 @@ class Binary_Search_Tree:
             # 4. Node to remove has a both a left subtree and a right subtree
             else:
                 node.value = self._most_left_node_from_right_subtree(node.right).value
-                self._remove_item(node, node.right, node.value)
+                self._remove(node, node.right, node.value)
         elif item < node.value:
-            self._remove_item(node, node.left, item)
+            self._remove(node, node.left, item)
         else:
-            self._remove_item(node, node.right, item)
+            self._remove(node, node.right, item)
     
     def _most_left_node_from_right_subtree(self, node):
         """ Returns the left-most node from the right subtree """
@@ -114,21 +112,9 @@ class Binary_Search_Tree:
             return node 
         else:
             return self._most_left_node_from_right_subtree(node.left)
-        
-    def _remove_item(self, parent, node, item):
-        if node.value == item:
-            if parent.left == node:
-                parent.left = None 
-            else:
-                parent.right = None
-                
-        elif item < node.value:
-            self._remove_item(node, node.left, item)
-        else:
-            self._remove_item(node, node.right, item)
 
     def empty(self):
-        """ Emptyies the tree """
+        """ Empties the tree """
         self.root = Node(None)
         
     def is_empty(self):
@@ -158,17 +144,6 @@ class Binary_Search_Tree:
         return node.value 
     
 def main():
-    t = Binary_Search_Tree()
-
-    print(t.is_empty())
-    t.add(10)
-    t.add(5)
-    print(t.is_empty()) 
-    t.empty()
-    print(t.is_empty())
-    print(t.get_max_value())
-    print(t.get_min_value())
-
     """
             10
           /    \
@@ -177,27 +152,19 @@ def main():
       2   8   14   30
     """
     t = Binary_Search_Tree()
-    t.add(10)
-    t.add(5)
-    t.add(20)
-    t.add(2)
-    t.add(8)
-    t.add(14)
-    t.add(30)
+    print("The tree is empty? ", t.is_empty())
 
-    t.search(5)
-    t.search(13)
-    t.search(20)
-    t.search(8)
+    testList = [10,5,20,2,8,14,30]
+    for i in testList:
+        t.add(i)
 
-    print(t.get_max_value())
-    print(t.get_min_value())
+    print("Max value: ",t.get_max_value())
+    print("Min value: ", t.get_min_value())
 
-    t.remove(2)
-    t.search(2)
-
-    t.remove(20)
-    t.search(20)
+    for j in testList:
+        t.remove(j)
+    print("The tree is empty? ", t.is_empty())
+    
 
 if __name__=="__main__":
     main()
